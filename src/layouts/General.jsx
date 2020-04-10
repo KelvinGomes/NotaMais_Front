264bf971b516886bc438267d2f50d84c989f1dcd
rivaelRodriguesJr <rivael.rodriguesjr@outlook.com>
2020-04-09 22:59:36 -0300

    Primeiro commit

---
+++ src/layouts/General.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+// javascript plugin used to create scrollbars on windows
+import PerfectScrollbar from "perfect-scrollbar";
+import { Route, Switch } from "react-router-dom";
+
+import DemoNavbar from "components/Navbars/DemoNavbar.jsx";
+import Footer from "components/Footer/Footer.jsx";
+import Sidebar from "components/Sidebar/Sidebar.jsx";
+import FixedPlugin from "components/FixedPlugin/FixedPlugin.jsx";
+
+import routes from "routes.js";
+
+var ps;
+
+class General extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundColor: "black",
+      activeColor: "info"
+    };
+    this.mainPanel = React.createRef();
+  }
+  
+  render() {
+    return (
+          <Switch>
+            {routes.map((prop, key) => {
+              return (
+                <Route
+                  path={prop.layout + prop.path}
+                  component={prop.component}
+                  key={key}
+                />
+              );
+            })}
+          </Switch>
+    );
+  }
+}
+
+export default General;
