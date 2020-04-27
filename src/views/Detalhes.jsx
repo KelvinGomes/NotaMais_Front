df6592d4d1cbdf124a488c737dd841ff75a42340
darasm <dara_souza.12@hotmail.com>
2020-04-26 23:35:26 -0300

    ajustes botão aceitar

--- src/views/Detalhes.jsx
+++ src/views/Detalhes.jsx
@@ -60,24 +60,31 @@ class Detalhes extends React.Component {
             <CardTitle className="titulo">Propóstas ofertadas</CardTitle>
             <Row>
                 <Card>
-                  <CardHeader>
+                  <CardHeader style={{backgroundColor: "rgb(58, 132, 177)", borderTopRightRadius: "15px", borderTopLeftRadius: "15px"}}>
 
                     <Row style={{textAlign: "center"}}>
                       <Col className="pr-1" md="3"><p style={{fontWeight: "bold"}}>Prestador</p><p>Amauri Junior</p></Col>
-                      <Col className="pr-1" md="2"><p style={{fontWeight: "bold"}}>Data</p><p>05-04-2020</p></Col>
-                      <Col className="pr-1" md="2"><p style={{fontWeight: "bold"}}>Valor</p><p>R$ 70,00</p></Col>
-                      <Col className="pr-1" md="4">
-                        <p style={{fontWeight: "bold"}}>Descrição</p>
+                      <Col className="pr-1" md="3"><p style={{fontWeight: "bold"}}>Data</p><p>05-04-2020</p></Col>
+                      <Col className="pr-1" md="3"><p style={{fontWeight: "bold"}}>Valor</p><p>R$ 70,00</p></Col>
+                      <Col className="pr-1" md="3">
+                        <a>
+                          <img src={require("assets/img/nota+/img-aceitar.png")}  alt = "aceitar"/>
+                          <p>Aceitar</p>
+                        </a>
+                        
+                      </Col>
+                    </Row>
+                  </CardHeader>
+
+                  <CardBody>
+                      <Col style={{textAlign: "center"}}>
+                        <p style={{fontWeight: "bold", color: "rgb(58, 132, 177)"}}>Descrição</p>
                         <p>Necessito de ajuda para realizar a formulação de 10 guidelines para um projeto, 
                            cada guideline deve conter sua descrição,
                             um exemplo e uma justificativa. Maiores informações se encontram no arquivo em anexo.</p>
                       </Col>
-                      <Col>
-                        <img src={require("assets/img/nota+/img-aceitar.png")}  alt = "aceitar"/>
-                      </Col>
-                    </Row>
-
-                  </CardHeader>
+                  </CardBody>
+                  
                 </Card>
             </Row>
           </Col>
