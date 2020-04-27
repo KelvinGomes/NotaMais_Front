b7ad405e0f5865daf5b5021fe22acc94d54950fe
darasm <dara_souza.12@hotmail.com>
2020-04-26 23:13:10 -0300

    ajuste css

--- src/views/Detalhes.jsx
+++ src/views/Detalhes.jsx
@@ -6,10 +6,8 @@ import {
   CardHeader,
   CardBody,
   CardTitle,
-  Table,
   CardGroup,
   Row,
-  CardText,
   CardFooter,
   Label,
   Col
@@ -20,17 +18,15 @@ class Detalhes extends React.Component {
     return (
       <>
         <div className="content">
-          <Col md="12" style={{display: "grid", flexDirection: "row", justifyContent: "center", alignItems: "center"}}>
-              <CardTitle style={{textAlign: "center", marginTop:"30px", fontSize: "20px", fontWeight: "bold"}}>Definição de guidelines para softwares</CardTitle>
-              <Row>
-                
-                <CardGroup>
-                  
+          <Col className = "coluna_descricao" md="12">
+              <CardTitle className="titulo">Definição de guidelines para softwares</CardTitle>
+              <Row>   
                   <Card>
                     <CardHeader style={{backgroundColor: "rgb(58, 132, 177)", borderTopRightRadius: "15px", borderTopLeftRadius: "15px"}}>
 
                       <Row style={{textAlign: "center"}}>
-                        <Col className="pr-1" md="3"><p style={{fontWeight: "bold"}}>Status</p><p>Concluído</p></Col>
+                        <Col className="pr-1" md="3"><p style={{fontWeight: "bold"}}>Status</p><p>Concluído</p>
+                        </Col>
                         <Col className="pr-1" md="3"><p style={{fontWeight: "bold"}}>Área</p><p>Ciências Exatas</p></Col>
                         <Col className="pr-1" md="3"><p style={{fontWeight: "bold"}}>Assunto</p><p>Desenvolvimento de sistemas</p></Col>
                         <Col className="pr-1" md="3"><p style={{fontWeight: "bold"}}>Prazo</p><p>10/04/2020</p></Col>
@@ -48,33 +44,28 @@ class Detalhes extends React.Component {
                     </CardBody>
                     <CardFooter >
                       <div style={{textAlign: "right"}}>
-                        <img src={require("assets/img/nota+/icone_arquivos.png")}  alt = "upload"/>
-                        <img src={require("assets/img/nota+/icone_pdf.png")}  alt = "pdf"/>
-                        <img src={require("assets/img/nota+/icone_foto.png")}  alt = "foto"/>
+                        <img className="icone_detalhe" src={require("assets/img/nota+/icone_arquivos.png")}  alt = "upload"/>
+                        <img className="icone_detalhe" src={require("assets/img/nota+/icone_pdf.png")}  alt = "pdf"/>
+                        <img className="icone_detalhe" src={require("assets/img/nota+/icone_foto.png")}  alt = "foto"/>
 
                       </div>
                     
                     </CardFooter>
                   </Card>
-                
-                </CardGroup>
               </Row>
 
           </Col>
 
-        <Col style={{display: "grid", flexDirection: "row", justifyContent: "center", alignItems: "center"}}  md="12">
-            <CardTitle style={{textAlign: "center", marginTop: "60px", marginBotton: "30px", fontSize: "20px", fontWeight: "bold"}}>Propóstas ofertadas</CardTitle>
+          <Col className = "coluna_descricao"  md="12">
+            <CardTitle className="titulo">Propóstas ofertadas</CardTitle>
             <Row>
-              
-              <CardGroup>
-                
                 <Card>
                   <CardHeader>
 
                     <Row style={{textAlign: "center"}}>
                       <Col className="pr-1" md="3"><p style={{fontWeight: "bold"}}>Prestador</p><p>Amauri Junior</p></Col>
                       <Col className="pr-1" md="2"><p style={{fontWeight: "bold"}}>Data</p><p>05-04-2020</p></Col>
-                      <Col className="pr-1" md="2"><p style={{fontWeight: "bold"}}>Valor</p><p> R$ 70,00</p></Col>
+                      <Col className="pr-1" md="2"><p style={{fontWeight: "bold"}}>Valor</p><p>R$ 70,00</p></Col>
                       <Col className="pr-1" md="4">
                         <p style={{fontWeight: "bold"}}>Descrição</p>
                         <p>Necessito de ajuda para realizar a formulação de 10 guidelines para um projeto, 
@@ -87,9 +78,7 @@ class Detalhes extends React.Component {
                     </Row>
 
                   </CardHeader>
-                  
                 </Card>
-              </CardGroup>
             </Row>
           </Col>
         </div>
