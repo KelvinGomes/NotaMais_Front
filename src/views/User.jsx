a00d9c527f57ef0d29c4b9a2c2ae63491e2c39a2
darasm <dara_souza.12@hotmail.com>
2020-05-31 15:44:20 -0300

    ajuste tela user

--- src/views/User.jsx
+++ src/views/User.jsx
@@ -21,7 +21,7 @@ class User extends React.Component {
     super(props);
     this.state = {
       user: {
-        contractor: true,
+        contractor: localStorage.getItem('contractor'),
         name: '',
         nickname: '',
         phone: '',
@@ -29,6 +29,7 @@ class User extends React.Component {
         area_interest: '',
         education_level: ''
       },
+      new_password: '',
       confirm_password: '',
       invalid_password: ''
     };
@@ -40,6 +41,8 @@ class User extends React.Component {
     this.tipoUsuario = this.tipoUsuario.bind(this);
   }
 
+  
+
   atribuirValor(event) {
     let user = this.state.user;
     user[event.target.name] = event.target.value;
@@ -50,7 +53,9 @@ class User extends React.Component {
   async submeter(event) {
     event.preventDefault();
     let user = this.state.user;
-    await axios.put(`https://notamais-backend01.herokuapp.com/users`, user)
+    let new_password = this.state.new_password;
+    
+    await axios.put(`https://notamais-backend01.herokuapp.com/users`, user,new_password)
         .then(res => {
             window.alert("Atualização efetuada com sucesso!");
             //window.location.href = "/general/login";
@@ -59,6 +64,7 @@ class User extends React.Component {
             window.alert("Erro ao atualizar perfil!");
         });
   }
+
   confirmarSenha() {
     let invalid_password = this.state.invalid_password;
     if (this.state.user.password != this.state.confirm_password) {
@@ -68,6 +74,8 @@ class User extends React.Component {
     }
     this.setState({ invalid_password: invalid_password });
   }
+
+  
 
   atribuirConfirmacao(event) {
     let state = this.state;
@@ -87,6 +95,8 @@ class User extends React.Component {
     await axios.get(`https://notamais-backend01.herokuapp.com/users`)
       .then(res => {
         let user = res.data.user;
+        user.contractor = localStorage.getItem('contractor');
+        console.log(user.contractor);
         this.setState({ user: user});
         console.log(res.data.user);
       })
@@ -110,8 +120,6 @@ class User extends React.Component {
   }
 
   render() {
-
-    if(this.state.user.contractor == true){
       return (
         <>
           <div className="content">
@@ -202,22 +210,31 @@ class User extends React.Component {
                         </Col>
                       </Row>
                       <Row>
-                        <Col className="pr-1" md="6">
+                      <Col className="pr-1" md="4">
                           <FormGroup>
-                            <label>Senha</label>
+                            <label>Senha Atual</label>
                             <Input
                               minLength="6" maxLength="10"
                               name="password" 
-                              value={this.state.password} 
-                              onChange={this.handleChange}
-                              value={this.state.user.password} 
+                              value={this.state.user.password}  
                               onChange={this.atribuirValor}
-                              placeholder="Senha"
                               type="password"
                             />
                           </FormGroup>
                         </Col>
-                        <Col className="pr-1" md="6">
+                        <Col className="pr-1" md="4">
+                          <FormGroup>
+                            <label>Nova Senha</label>
+                            <Input
+                              minLength="6" maxLength="10"
+                              name="new_password" 
+                              value={this.state.new_password} 
+                              onChange={this.handleChange}
+                              type="password"
+                            />
+                          </FormGroup>
+                        </Col>
+                        <Col className="pr-1" md="4">
                           <FormGroup>
                             <label>Confirmar Senha</label>
                             <Input
@@ -225,47 +242,52 @@ class User extends React.Component {
                               name="confirm_password" 
                               value={this.state.confirm_password} 
                               onChange={this.handleChange}
-                              placeholder="Confirmar Senha"
+                              
                               type="password"
                             />
                           </FormGroup>
                         </Col>
                       </Row>
-                      <Row>
-                        <Col className="pr-1" md="6">
-                          <FormGroup>
-                            <label>Grau de intrução</label>
-                            <br />
-                            <Input type="select" name="select" 
-                              placehold={this.state.user.education_level} 
-                              id="exampleSelect"
-                              name="education_level" 
-                              value={this.state.user.education_level} 
-                              onChange={this.atribuirValor}>
-                              <option value="1">Ensino médio</option>
-                              <option value="2">Técnico</option>
-                              <option value="3">Ensino superior</option>
-                            </Input>
-                          </FormGroup>
-                        </Col>
-                        <Col className="pr-1" md="6">
-                          <FormGroup>
-                            <label>Área de Interesse</label>
-                            <br />
-                            <Input type="select" name="select" 
-                                placehold={this.state.user.area_interest}
-                                name="area_interest" 
-                                value={this.state.user.area_interest} 
-                                onChange={this.atribuirValor} 
-                                id="exampleSelect">
-                                <option value="1">Ciências Exatas</option>
-                                <option value="2">Ciencias Humanas</option>
-                                <option value="3">Ciências Biológicas</option>
-                                <option value="4">Linguagens e Códigos</option>
-                            </Input>
-                          </FormGroup>
-                        </Col>
-                      </Row>
+                      {this.state.user.contractor === 'false' &&(
+
+                          <Row>
+                          <Col className="pr-1" md="6">
+                            <FormGroup>
+                              <label>Grau de intrução</label>
+                              <br />
+                              <Input type="select" name="select" 
+                                placehold={this.state.user.education_level} 
+                                id="exampleSelect"
+                                name="education_level" 
+                                value={this.state.user.education_level} 
+                                onChange={this.atribuirValor}>
+                                <option value="1">Ensino médio</option>
+                                <option value="2">Técnico</option>
+                                <option value="3">Ensino superior</option>
+                              </Input>
+                            </FormGroup>
+                          </Col>
+                          <Col className="pr-1" md="6">
+                            <FormGroup>
+                              <label>Área de Interesse</label>
+                              <br />
+                              <Input type="select" name="select" 
+                                  placehold={this.state.user.area_interest}
+                                  name="area_interest" 
+                                  value={this.state.user.area_interest} 
+                                  onChange={this.atribuirValor} 
+                                  id="exampleSelect">
+                                  <option value="1">Ciências Exatas</option>
+                                  <option value="2">Ciencias Humanas</option>
+                                  <option value="3">Ciências Biológicas</option>
+                                  <option value="4">Linguagens e Códigos</option>
+                              </Input>
+                            </FormGroup>
+                          </Col>
+                          </Row>
+
+                      )}
+                      
   
                       <Row>
                         <div className="update ml-auto mr-auto">
@@ -289,150 +311,6 @@ class User extends React.Component {
 
       );
 
-    }
-    if (this.state.user.contractor == false){
-      return (
-        <>
-          <div className="content">
-            <Row>
-              <Col md="4" style={{ marginBottom: "30px" }}>
-                <Card style={{ height: "100%", maxHeight: "615px" }} className="card-user">
-                  <div className="image">
-                    <img
-                      alt="..."
-                      src={require("assets/img/damir-bosnjak.jpg")}
-                    />
-                  </div>
-                  <CardBody>
-                    <div className="author">
-                      <a href="#pablo" onClick={e => e.preventDefault()}>
-                        <img
-                          alt="..."
-                          className="avatar border-gray"
-                          src={require("assets/img/mike.jpg")}
-                        />
-                        <h5 className="title">{this.state.user.name}</h5>
-                      </a>
-                      <p className="description">@chetfaker</p>
-                    </div>
-                    <p style={{ height: "100%", maxHeight: "500px" }} className="description text-center">
-                      "Apaixonado por disceminar conhecimento"<br />
-                    </p>
-                  </CardBody>
-                  <CardFooter style={{ backgroundColor: "rgb(58, 132, 177)", height: "80px", borderBottomLeftRadius: "10px", borderBottomRightRadius: "10px" }}>
-                    <div style={{ textAlign: "center", marginTop: "30px" }}>
-                      <CardLink href="/admin/quiz_descricao">Definir Perfil Estudantil</CardLink>
-                    </div>
-                  </CardFooter>
-                </Card>
-              </Col>
-              <Col md="8">
-                <Card className="card-user">
-                  <CardHeader>
-                    <CardTitle tag="h5">Editar Perfil</CardTitle>
-                  </CardHeader>
-                  <CardBody style={{ marginRight: "10px" }}>
-                    <Form >
-                      <Row>
-                        <Col className="pr-1" md="12">
-                          <FormGroup>
-                            <label>Nome</label>
-                            <Input
-                              name="name"
-                              value={this.state.user.name} 
-                              onChange={this.atribuirValor}
-                              placeholder={this.state.user.name}
-                              onChange={this.atribuirValor}
-                              type="text"
-                              require 
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col className="pr-1" md="6">
-                          <FormGroup>
-                            <label>Apelido</label>
-                            <Input
-                              name="nickname"
-                              value={this.state.user.nickname} 
-                              onChange={this.atribuirValor}
-                              placeholder={this.state.user.nickname}
-                              type="text"
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col className="pr-1" md="6">
-                          <FormGroup>
-                            <label>Telefone</label>
-                            <Input
-                              name="phone"
-                              value={this.state.user.phone} 
-                              onChange={this.atribuirValor}
-                              placeholder={this.state.user.phone}
-                              type="text"
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col className="pr-1" md="12">
-                          <FormGroup>
-                            <label htmlFor="exampleInputEmail1">
-                              Email address
-                            </label>
-                            <Input placeholder={this.state.user.email} type="email" disabled="true" />
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                      <Row>
-                        <Col className="pr-1" md="6">
-                          <FormGroup>
-                            <label>Senha</label>
-                            <Input
-                              minLength="6" maxLength="10" name="password" 
-                              value={this.state.user.password} 
-                              onChange={this.atribuirValor}
-                              placeholder="Senha"
-                              type="password"
-                            />
-                          </FormGroup>
-                        </Col>
-                        <Col className="pr-1" md="6">
-                          <FormGroup>
-                            <label>Confirmar Senha</label>
-                            <Input
-                              minLength="6" maxLength="10"
-                              name="confirm_password" 
-                              value={this.state.confirm_password} 
-                              onChange={this.handleChange}
-                              placeholder="Confirmar Senha"
-                              type="password"
-                            />
-                          </FormGroup>
-                        </Col>
-                      </Row>  
-  
-                      <Row>
-                        <div className="update ml-auto mr-auto">
-                          <Button
-                            className="btn_registre"
-                            onClick={this.submeter}
-                            color="primary"
-                            type="submit"
-                          >
-                            Salvar
-                          </Button>
-                        </div>
-                      </Row>
-                    </Form>
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
-          </div>
-        </>
-
-
-      );
-    }
-    
   }
 }
 
