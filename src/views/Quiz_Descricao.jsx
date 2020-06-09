fc811c1f97993603c0cf17d1d6636d36704eb249
darasm <dara_souza.12@hotmail.com>
2020-06-08 21:31:12 -0300

    atualização tela quiz de descrição

--- src/views/Quiz_Descricao.jsx
+++ src/views/Quiz_Descricao.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from 'axios';
 
 import {
   Button,
@@ -17,6 +18,49 @@ import {
 } from "reactstrap";
 
 class Quiz_Descricao extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        name: '',
+        email: '',
+        nickname: '',
+        phone: '',
+        password: '',
+        confirmPassword: '',
+        oldPassword: '', 
+        area_interest: '', 
+        education_level: ''
+        
+      },
+      contractor: localStorage.getItem('contractor'),
+    };
+    this.tipoUsuario = this.tipoUsuario.bind(this);
+  }
+
+  async componentDidMount() {
+
+    let token = await localStorage.getItem('token');
+    axios.defaults.headers.common = { 'Authorization': `bearer ${token}` }
+    await axios.get(`https://notamais-backend01.herokuapp.com/users`)
+      .then(res => {
+        let user = res.data.user;
+        user.contractor = localStorage.getItem('contractor');
+        
+        this.setState({ user: user});
+        console.log(res.data.user);
+      })
+    
+  }
+  tipoUsuario() {
+    var texto = '';
+    if(this.state.contractor === 'true'){
+      return texto = "No momento seu usuário é do tipo Aluno!";
+    }else{
+      return texto = "No momento seu usuário é do tipo Instrutor!";
+    }
+  }
+
   render() {
     return (
       <>
@@ -33,17 +77,17 @@ class Quiz_Descricao extends React.Component {
                 <CardBody>
                   <div className="author">
                     <a href="#pablo" onClick={e => e.preventDefault()}>
-                      <img
-                        alt="..."
-                        className="avatar border-gray"
-                        src={require("assets/img/mike.jpg")}
-                      />
-                      <h5 className="title">Chet Faker</h5>
+                        <img 
+                            className="avatar border-gray" 
+                            src={require("assets/img/nota+/Profile.png")}
+                        />
+                      <h5 className="title">{this.state.user.name}</h5>
                     </a>
-                    <p className="description">@chetfaker</p>
+                    <p className="description">@ {this.state.user.nickname}</p>
                   </div>
-                  <p style={{ height: "100%", maxHeight: "500px" }} className="description text-center">
-                    "Apaixonado por disceminar conhecimento"<br />
+              
+                  <p style={{ height: "100%"}} className="description text-center">
+                      {this.tipoUsuario()}
                   </p>
                 </CardBody>
                 <CardFooter style={{ backgroundColor: "rgb(58, 132, 177)", height: "80px", borderBottomLeftRadius: "10px", borderBottomRightRadius: "10px" }}>
